ed800c7f88f881471440c7bfa12d451bfda64765
shreyakotagal <117657204+shreyakotagal@users.noreply.github.com>
2023-10-24 20:55:33 +0530

    Update pes_graycode_tb.v

--- pes_graycode_tb.v
+++ pes_graycode_tb.v
@@ -1,54 +1,21 @@
 module pes_graycode_tb();
-
-    reg clk;
-    reg enable;
-    reg reset;
+    reg clk, enable, reset;
     wire [7:0] gray_count;
-
-    // Instantiate the iiitb_gc module
-    pes_graycode uut (
-        .clk(clk),
-        .enable(enable),
-        .reset(reset),
-        .gray_count(gray_count)
-    );
-
-    // Clock generation
-    always
-    begin
-        #5 clk = ~clk;
-    end
-
-    // Stimulus generation
+    
+    pes_graycode gc1(clk, enable, reset, gray_count);
+    
     initial
     begin
+        $dumpfile("pes_graycode.vcd");
+        $dumpvars(0, pes_graycode_tb);
         clk = 0;
         reset = 1;
-        enable = 0;
-        #10 reset = 0;
-
-        // Test case 1: Perform some operations
         enable = 1;
-        #20 enable = 0;
-
-        // Test case 2: Reset
-        reset = 1;
-        #10 reset = 0;
-        #20 reset = 1;
-        #10 reset = 0;
-        enable = 1;
-        #20 enable = 0;
-
-        // Add more test cases here as needed
-
-        // Stop the simulation after some time
-        #100 $finish;
-    end
-
-    // Display gray_count for monitoring
-    always @(posedge clk)
-    begin
-        $display("gray_count = %h", gray_count);
-    end
-
+        
+        #5 reset = 0;
+        #4000 $finish;     
+    end 
+    
+    always #2 clk = ~clk;
+       
 endmodule
